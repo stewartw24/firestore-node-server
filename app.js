ae9343b7320f61ca2dc1d1f0692980fc99b062d8
William <stewart-w24@hotmail.co.uk>
2023-06-23 17:41:18 +0100

    cors fix

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const cors = require("cors");
-const config = require("./config");
-const contactRoutes = require("./routes/contact-routes");
-const feedbackRoutes = require("./routes/feedback-routes");
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+const contactRoutes = require('./routes/contact-routes');
+const feedbackRoutes = require('./routes/feedback-routes');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://charming-boba-83a35f.netlify.app',
+  }),
+);
 
-app.use("/api", contactRoutes.routes);
-app.use("/api", feedbackRoutes.routes);
+app.use('/api', contactRoutes.routes);
+app.use('/api', feedbackRoutes.routes);
 
 app.listen(process.env.PORT || 3000);
