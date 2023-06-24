c0d5740798daa6ace7135499915cb50edaa61685
William <stewart-w24@hotmail.co.uk>
2023-06-24 14:27:35 +0100

    cors fix

--- app.js
+++ app.js
@@ -9,11 +9,7 @@ const feedbackRoutes = require('./routes/feedback-routes');
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'https://portfolio--charming-boba-83a35f.netlify.app',
-  }),
-);
+app.use(cors());
 
 app.options('*', cors());
 
