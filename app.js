776639bf815589c6b3c972e156c338095dc34f3d
William <stewart-w24@hotmail.co.uk>
2023-06-24 13:13:29 +0100

    test to fix connection

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://portfolio--charming-boba-83a35f.netlify.app',
-    credentials: true,
+    origin: '*',
   }),
 );
 
